aff4ddf8ac355d53d17d890129adabdf1e1f23bb
mohamedhossam12rd-mh <mohamedhossam12rd@gamil.com>
2026-01-06 18:52:08 +0200

    updated

--- routers/UserRoutes.js
+++ routers/UserRoutes.js
@@ -3,8 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 const UserController = require("../controllers/UserControllers");
-const uploader = require("../../../epress-test/express/utils/uploadeServices");
-
+// const uploader = require("../../../epress-test/express/utils/uploadeServices");
+const {uploader} = require("../utils/userServices");
 router.get("", UserController.findAll);
 router.get("/:id", UserController.findOne);
 router.post("", 
